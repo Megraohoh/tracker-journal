25e01075db2f66d2d16968b9d7d208dcb1bfdf24
Meghan Debity <mdebity@gmail.com>
2018-06-13 14:18:49 -0500

    navbar in place

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,7 @@
 const loginComponent = require("./login")
 const registerComponent = require("./register")
+const journalComponent = require("./entries/journal")
+
+journalComponent()
 loginComponent()
 registerComponent()
