d73be1df2a3431f42526bea38b8470d55391bca8
Meghan Debity <mdebity@gmail.com>
2018-06-13 15:06:18 -0500

    entries API manager added

---
+++ src/apiManager/entriesApiManager.js
@@ -0,0 +1,47 @@
+const $ = require("jquery")
+
+const entryManager = Object.create(null, {
+    //get all entries
+    getAllEntries: {
+        value: function() {
+            return $.ajax("http://localhost:8088/entries")
+        }
+    },
+    //get single entry
+    getSingleEntry: {
+        value: function(id){
+            return $.ajax(`http://localhost:8088/entries/${id}`)
+        }
+    },
+    //post entry to database
+    createEntry: {
+        value: function(Entries){
+            return $.ajax({
+                url: "http://localhost:8088/entries",
+                method: "POST",
+                data: Entries
+            })
+        }
+    },
+    //delete entry
+    deleteEntry: {
+        value: function(id){
+            return $.ajax({
+                url: `http://localhost:8088/entries/${id}`,
+                method: "DELETE"
+            })
+        }
+    },
+    //put (update) user
+    updateEntry: {
+        value: function(Entries){
+            return $.ajax({
+                url: `http://localhost:8088/entries/${Entries.id}`,
+                method: "PUT",
+                data: Entries
+            })
+        }
+    }
+})
+
+module.exports = entryManager
