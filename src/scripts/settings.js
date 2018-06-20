39ba332b2274f34293ff8db273dc0be4e5ce4eef
Meghan Debity <mdebity@gmail.com>
2018-06-20 11:26:06 -0500

    grabbing user id from session storage for further use to save settings to user

--- src/scripts/settings.js
+++ src/scripts/settings.js
@@ -1,8 +1,9 @@
 //Purpose: to display settings options when nav option selected
 const $ = require("jquery")
 const  userManager = require("../apiManager/userApiManager")
+const session = require("../apiManager/activeUserApiManager")
     //ALL PROFILE AND SETTINGS SHOULD BE FILLED OUT AND STAY AS DEFAULT TO THE USER LOGGED IN
- 
+   
 //first setting to create is to hide text in journal input
 const hideJournalText = function() { 
     $("#settings").click(function(e){
@@ -12,9 +13,31 @@ const hideJournalText = function() {
 
     $("input:checkbox").change(function(){
         if ($(this).is(":checked")) {
-            console.log("is checked")
             return document.getElementById("journalText").setAttribute("type", "password")
         }
     })
+
+    $("input:checkbox").change(function() {
+        if ($(this).is(":checked")) {
+            let user = JSON.parse(sessionStorage.getItem("ActiveUser")).then(user => {
+                user.forEach( e => {
+                    
+                })
+            })
+
+            userManager.getSingleusers("user").then(userId => {})
+            }
+
+            //getsingleuser.then -> add settings then updateuser
+            
+            // userManager.updateusers({
+       
+            //     "settings": {
+            //         "hideText": true
+            //     }
+            // })
+        })
+    
+
 }
 module.exports = hideJournalText
